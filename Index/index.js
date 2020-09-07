d5a1d7b0fe7bb06308b4d6dc8b7067476ab5c9fa
KSClopton <kyle.clopton@outlook.com>
2020-09-07 15:20:04 -0400

    Finished the coniditions

--- Index/index.js
+++ Index/index.js
@@ -25,35 +25,26 @@ server.get('/', (req, res) => {
 
 server.post('/api/users', (req, res) => {
     const newUser = req.body; 
-    users.add(newUser)
-    .then(user => {
-  
-    if(newUser !== req.body.json({name: "", bio: ""})){
-        res.status(400).json({errorMessage: "Please provide name and bio for the user."})
-    }else {
+    if(!newUser.name || !newUser.bio){
+        res.status(40).json({errorMessage: "please include name and bio"})
+    }else{
         newUser.id = shortid.generate();
         users.push(newUser);
-         
-    }})
-    .catch(error => {
-        console.log(error);
-        res.status(500).json({
-            errorMessage: "There was an error while saving the user to the database" 
-        });
-    })
-    .then(
-        res.status(201).json(newUser)
-    )
-    .catch(
-        res.status(500).json({
-            errorMessage: "The users information could not be retrieved."
-        }));
-    
+        res.json(users)
+        .then(worked => {
+            res.status(201)
+        })
+        .catch(error =>{
+            res.status(500).json({errorMessage: "There was an error while saving the user to the database."})
+        })
+    }
+
 })
+
 server.get('/api/users', (req, res) => {
-    users.find(req.query)
+    res.json(users)
     .then(users => {
-        res.status(200).json(users);
+        res.status(200);
     })
     .catch(error => {
         res.status(500).json({
@@ -64,43 +55,75 @@ server.get('/api/users', (req, res) => {
 
 server.get('/api/users/:id', (req, res) => {
     id = req.params.id
-    users.find(id)
-    .then(user => {
-        if (user) {
-            res.status(200).json(user);
-        }else{
-            res.status(404).json({errorMessage: 'User not found'})
-        }
+    let found = users.filter(user => user.id === id)
+    
+    if(found.length === 1) {
+        res.status(200).json(found)
+        .then(user => {
+            
+        })
+        .catch(error => {
+            res.status(500).json({errorMessage: "The information could not be retrieved"})
+        })
+    } else {
+        res.status(404).json({errorMessage: 'User not found'})
+    }
     })
-    .catch(error => {
-        res.status(500).json({errorMessage: "The information could not be retrieved"})
-    })
-})
 server.delete('/api/users/:id', (req, res) => {
     const id = req.params.id
-    const deleted = users.find(user => user.id === id);
+    const deleted = users.filter(user => user.id === id);
     let found = users.filter(user => user.id === id)
-    if(found){
-       users.filter(user => user.id !== id); 
-       res.status(204).json(found);
+    if(found.length === 1){
+
+       res.status(200).json(users.filter(user => user.id !== id))
+
+       .then(
+       )
+       .catch(error => {
+           res.status(500).json({errorMessage: "The user could not be removed."})
+       })
     }else{
         res.status(404).json({message: "User not found"})
+        res.json(found)
     }
     
 })
-server.put('/api/users/:id', (req, res) => {
-    const id = req.params.id
-    const changes = req.body;
-    let found = users.filter(user => user.id === id);
+// server.put('/api/users/:id', (req, res) => {
+//     const id = req.params.id
+//     const changes = req.body;
+//     let found = users.filter(user => user.id === id);
 
-    if(found){
+//     if(found){
+//         Object.assign(found, changes);
+//         res.status(200).json(found)
+//     } else {
+//         res.status(404).json({ message: "User not found"});
+//     }
+    
+//     res.json(found);
+// })
+
+server.put('/api/users/:id', (req, res) => {
+    id = req.params.id
+    const updateUser = req.body
+    let found = users.filter(user => user.id === id)
+    const changes = req.body
+
+    if(!updateUser.name || !updateUser.bio){
+        res.status(400).json({errorMessage: "please include name and bio"})
+    } else if(found.length !== 1){
+        res.status(404).json({errorMessage: "The user with the specified ID does not exist."})
+    }else {
         Object.assign(found, changes);
-        res.status(200).json(found)
-    } else {
-        res.status(404).json({ message: "User not found"});
+        res.json(users)
+        .then(worked => {
+            res.status(200)
+        })
+        .catch(error => {
+            res.status(500).json({errorMessage: "The user information could not be modified."})
+        })
     }
     
-    res.json(found);
 })
 
 const PORT = 5000; 
